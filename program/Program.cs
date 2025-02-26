a85f54c6adf98a29ca0e7c7010f5eeebfc6cc0d9
StaticCloud <diegomarrs@gmail.com>
2025-02-26 12:08:19 -0800

    completed Dueling Traditions

--- program/Program.cs
+++ program/Program.cs
@@ -1,80 +1,90 @@
-﻿// Level 8 Challenge
-// DefenseOfConsolas defenseOfConsolas = new DefenseOfConsolas();
+﻿
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Level 8 Challenge
+            // DefenseOfConsolas defenseOfConsolas = new DefenseOfConsolas();
 
-// Level 9 Challenge
-// RepairingTheClocktower repairingTheClocktower = new RepairingTheClocktower();
+            // Level 9 Challenge
+            // RepairingTheClocktower repairingTheClocktower = new RepairingTheClocktower();
 
-// Level 9 Challenge
-// Watchtower watchtower = new Watchtower();
+            // Level 9 Challenge
+            // Watchtower watchtower = new Watchtower();
 
-// Level 10 Challenge
-// BuyingInventory buyingInventory = new BuyingInventory();
+            // Level 10 Challenge
+            // BuyingInventory buyingInventory = new BuyingInventory();
 
-// Level 11 Challenge
-// ThePrototype thePrototype = new ThePrototype();
+            // Level 11 Challenge
+            // ThePrototype thePrototype = new ThePrototype();
 
-// Level 11 Challenge
-// TheMagicCannon theMagicCannon = new TheMagicCannon();
+            // Level 11 Challenge
+            // TheMagicCannon theMagicCannon = new TheMagicCannon();
 
-// Level 12 Challenge
-// ReplicatorOfDto replicatorOfDto = new ReplicatorOfDto();
+            // Level 12 Challenge
+            // ReplicatorOfDto replicatorOfDto = new ReplicatorOfDto();
 
-// Level 12 Challenge
-// TheLawsOfFrench theLawsOfFrench = new TheLawsOfFrench();
+            // Level 12 Challenge
+            // TheLawsOfFrench theLawsOfFrench = new TheLawsOfFrench();
 
-// Level 13 Challenge
-// TakingANumber takingANumber = new TakingANumber();
+            // Level 13 Challenge
+            // TakingANumber takingANumber = new TakingANumber();
 
-// Level 13 Challenge
-// Countdown countdown = new Countdown();
+            // Level 13 Challenge
+            // Countdown countdown = new Countdown();
 
-// Level 14 Boss Battle
-// HuntingTheManicore huntingTheManicore = new HuntingTheManicore();
+            // Level 14 Boss Battle
+            // HuntingTheManicore huntingTheManicore = new HuntingTheManicore();
 
-// Level 16 Challenge
-// SimulasTest simulasTest = new SimulasTest();
+            // Level 16 Challenge
+            // SimulasTest simulasTest = new SimulasTest();
 
-// Level 17 Challenge
-// SimulasSoup simulasSoup = new SimulasSoup();
+            // Level 17 Challenge
+            // SimulasSoup simulasSoup = new SimulasSoup();
 
-// Level 18
-// VinFletchersArrows vinFletchersArrows = new VinFletchersArrows();
+            // Level 18
+            // VinFletchersArrows vinFletchersArrows = new VinFletchersArrows();
 
-// Level 24 Boss Battle
-// ThePoint thePoint = new ThePoint();
+            // Level 24 Boss Battle
+            // ThePoint thePoint = new ThePoint();
 
-// Level 24 Boss Battle
-// TheColor theColor = new TheColor();
+            // Level 24 Boss Battle
+            // TheColor theColor = new TheColor();
 
-// Level 24 Boss Battle
-// TheCard theCard = new TheCard();
+            // Level 24 Boss Battle
+            // TheCard theCard = new TheCard();
 
-// Level 24 Boss Battle
-// TheLockedDoor theLockedDoor = new TheLockedDoor();
+            // Level 24 Boss Battle
+            // TheLockedDoor theLockedDoor = new TheLockedDoor();
 
-// Level 24 Boss Battle
-// ThePasswordValidator thePasswordValidator = new ThePasswordValidator();
+            // Level 24 Boss Battle
+            // ThePasswordValidator thePasswordValidator = new ThePasswordValidator();
 
-// Level 24 Boss Battle
-// TicTacToe ttt = new TicTacToe();
+            // Level 24 Boss Battle
+            // TicTacToe ttt = new TicTacToe();
 
-// Level 25 Challenge
-// PackingInventory packingInventory = new PackingInventory();
+            // Level 25 Challenge
+            // PackingInventory packingInventory = new PackingInventory();
 
-// Level 26
-// TheOldRobot theOldRobot = new TheOldRobot();
+            // Level 26
+            // TheOldRobot theOldRobot = new TheOldRobot();
 
-// Level 28
-// RoomCoordinates roomCoordinates = new RoomCoordinates();
+            // Level 28
+            // RoomCoordinates roomCoordinates = new RoomCoordinates();
 
-// Level 29
-// WarPreparations warPreparations = new WarPreparations();
+            // Level 29
+            // WarPreparations warPreparations = new WarPreparations();
 
-// Level 30
-// ColoredItems coloredItems = new ColoredItems();
+            // Level 30
+            // ColoredItems coloredItems = new ColoredItems();
 
-// Level 31
-// TheFountainOfObjects theFountainOfObjects = new TheFountainOfObjects();
+            // Level 31
+            // TheFountainOfObjects theFountainOfObjects = new TheFountainOfObjects();
 
-// Level 33
-TheFeud theFeud = new TheFeud();
+            // Level 33
+            TheFeud theFeud = new TheFeud();
+        }
+    }
+}
