a5dfcbbf8dccb40d3240bdc01ca50a4ccff388dc
StaticCloud <diegomarrs@gmail.com>
2025-02-28 21:21:44 -0800

    completed The Lambda Sieve

--- program/TheSieve.cs
+++ program/TheSieve.cs
@@ -15,35 +15,27 @@
 
             int number = Convert.ToInt32(Console.ReadLine());
 
-            Predicate<int> func = option switch
+            Sieve sieve = option switch
             {
-                1 => Sieve.IsEven,
-                2 => Sieve.IsPositive,
-                3 => Sieve.IsMultiple,
-                _ => Sieve.IsEven
+                1 => new Sieve(n => n % 2 == 0),
+                2 => new Sieve(n => n > 0),
+                3 => new Sieve(n => n % 10 == 0),
+                _ => new Sieve(n => n % 2 == 0)
             };
 
-            Sieve sieve = new Sieve(func);
             Console.WriteLine(sieve.IsGood(number));
         }
     }
 
     class Sieve
     {
-        Predicate<int> Func;
+        Func<int, bool> Func;
 
-        public Sieve(Predicate<int> func)
-        { 
+        public Sieve(Func<int, bool> func)
+        {
             Func = func;
         }
 
-        public bool IsGood(int number)
-        {
-            return Func(number);
-        }
-
-        public static Predicate<int> IsEven = (num) => num % 2 == 0;
-        public static Predicate<int> IsPositive = (num) => num > 0;
-        public static Predicate<int> IsMultiple = (num) => num % 10 == 0;
+        public bool IsGood(int number) => Func(number);
     }
 }
